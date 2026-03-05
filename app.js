0779d0b54cfab766fdc547609bdb749ee372b942
kabir mondal <mirajsk20002gmail.com>
2026-03-05 14:26:46 +0530

    kabir

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 const express = require('express');
 const cors = require('cors');
-const dotenv = require('dotenv');
-
-dotenv.config();
 
 const app = express();
 
@@ -20,12 +17,6 @@ app.get('/', (req, res) => {
 app.use((err, req, res, next) => {
   console.error(err.stack);
   res.status(500).json({ error: 'Something went wrong' });
-});
-
-// Server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
 });
 
 module.exports = app;
